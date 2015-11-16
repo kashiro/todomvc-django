3bc22e8a6886325cb7c75446cc5125bcc0533c7a
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2015-11-16 21:21:59 +0900

    show model in admin site

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from todo.models import Todo
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completed', )
+
+admin.site.register(Todo, TodoAdmin)
